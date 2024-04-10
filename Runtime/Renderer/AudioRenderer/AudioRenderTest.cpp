13950e41755111808af952df578edbdafa0851bb
Cheesecuter <Cheesecuter777@gmail.com>
2024-04-10 23:39:36 +0800

    v0.0.1

--- Runtime/Renderer/AudioRenderer/AudioRenderTest.cpp
+++ Runtime/Renderer/AudioRenderer/AudioRenderTest.cpp
@@ -3,48 +3,67 @@
 #include <AL/alut.h>
 
 #include <iostream>
+#include <conio.h>
 
 int main(int argc, char** argv)
 {
+	bool paused = false, stopped = false;
+
 	alutInit(&argc, argv);
 
-	ALCdevice* device = alcOpenDevice(NULL);
-	if (device != NULL)
+	ALuint source;
+	alGenSources(1, &source);
+
+	ALuint buffer = alutCreateBufferFromFile("D:/SporeEngine/master/SporeEngine/Assets/Audios/Accidents_Will_Happen.wav");
+
+	alSourcei(source, AL_BUFFER, buffer);
+
+	alSourcePlay(source);
+
+	ALenum error = alGetError();
+	if (error != AL_NO_ERROR)
 	{
-		ALCcontext* context = alcCreateContext(device, NULL);
-		if (context != NULL)
-		{
-			alcMakeContextCurrent(context);
-
-			ALuint buffer;
-			alGenBuffers(1, &buffer); 
-			//buffer = alutCreateBufferFromFile("./Assets/Audios/ES_Maximum High 3 - Jack Elphick.wav");
-			//buffer = alutCreateBufferFromFile("D:/SporeEngine/master/SporeEngine/Assets/Audios/ES_Maximum High 3 - Jack Elphick.wav");
-			buffer = alutCreateBufferFromFile("D:/SporeEngine/master/SporeEngine/Assets/Audios/helloworld.wav");
-			ALuint source;
-			alGenSources(1, &source);
-			alSourcei(source, AL_BUFFER, buffer);
-			alSourcef(source, AL_GAIN, 0.8f);
-
-			alSourcePlay(source);
-			ALint state;
-			do
-			{
-				alGetSourcei(source, AL_SOURCE_STATE, &state);
-			}
-			while (state == AL_PLAYING);
-
-			//alSourceStop(source);
-
-			alDeleteSources(1, &source);
-
-			alDeleteBuffers(1, &buffer);
-
-			alcDestroyContext(context);
-		}
-
-		alcCloseDevice(device);
+		std::cerr << "Error occurred: " << alGetString(error) << std::endl;
+		return 1;
 	}
+
+	while (!stopped)
+	{
+		if (_kbhit())
+		{
+			char key = _getch();
+			if (key == 'p' || key == 'P')
+			{
+				if (!paused)
+				{
+					alSourcePause(source);
+					paused = true;
+				}
+				else
+				{
+					alSourcePlay(source);
+					paused = false;
+				}
+			}
+			else if (key == 's' || key == 'S')
+			{
+				alSourceStop(source);
+				stopped = true;
+			}
+		}
+	}
+
+	ALint source_state;
+	alGetSourcei(source, AL_SOURCE_STATE, &source_state);
+	if (source_state != AL_PLAYING)
+	{
+		stopped = true;
+	}
+
+	alDeleteSources(1, &source);
+	alDeleteBuffers(1, &buffer);
+
+	alutExit();
 
 	return 0;
 }
