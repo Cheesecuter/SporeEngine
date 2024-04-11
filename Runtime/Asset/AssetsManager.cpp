adaa2c9e74e4f58d2a1ed9aa540e535055b4a5e7
Cheesecuter <Cheesecuter777@gmail.com>
2024-04-12 00:15:46 +0800

    Added management of Audio asset files.

--- Runtime/Asset/AssetsManager.cpp
+++ Runtime/Asset/AssetsManager.cpp
@@ -5,10 +5,12 @@ namespace Spore
 	class Shader;
 	class Texture;
 	class Model;
+	class Audio;
 
 	std::map<std::string, Shader*> AssetsManager::m_shader_mapper;
 	std::map<std::string, Texture*> AssetsManager::m_texture_mapper;
 	std::map<std::string, Model*> AssetsManager::m_model_mapper;
+	std::map<std::string, Audio*> AssetsManager::m_audio_mapper;
 	std::unordered_map<std::string, int32> AssetsManager::m_model_counter;
 
 	AssetsManager::AssetsManager()
--- Runtime/Asset/AssetsManager.hpp
+++ Runtime/Asset/AssetsManager.hpp
@@ -8,6 +8,7 @@ namespace Spore
 	class Shader;
 	class Texture;
 	class Model;
+	class Audio;
 
 	class AssetsManager : public Singleton<AssetsManager>
 	{
@@ -15,6 +16,7 @@ namespace Spore
 		static std::map<std::string, Shader*> m_shader_mapper;
 		static std::map<std::string, Texture*> m_texture_mapper;
 		static std::map<std::string, Model*> m_model_mapper;
+		static std::map<std::string, Audio*> m_audio_mapper;
 		static std::unordered_map<std::string, int32> m_model_counter;
 
 	protected:
