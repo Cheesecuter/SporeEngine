37039c95766e5e1737d1e809217047d907b8a7dc
Cheesecuter <Cheesecuter777@gmail.com>
2024-04-16 16:32:06 +0800

    Add Gizmo Transform Control.

--- Runtime/Utils/Gizmos.cpp
+++ Runtime/Utils/Gizmos.cpp
@@ -41,26 +41,12 @@ namespace Spore
 			{
 				m_current_gizmo_operation = ImGuizmo::SCALE;
 			}
-			/*if (ImGui::RadioButton("Position", m_current_gizmo_operation == ImGuizmo::TRANSLATE))
-			{
-				m_current_gizmo_operation = ImGuizmo::TRANSLATE;
-			}
-			if (ImGui::RadioButton("Rotation", m_current_gizmo_operation == ImGuizmo::ROTATE))
-			{
-				m_current_gizmo_operation = ImGuizmo::ROTATE;
-			}
-			if (ImGui::RadioButton("Scale", m_current_gizmo_operation == ImGuizmo::SCALE))
-			{
-				m_current_gizmo_operation = ImGuizmo::SCALE;
-			}*/
-			ImGuizmo::DecomposeMatrixToComponents(p_matrix, glm::value_ptr(m_position), glm::value_ptr(m_rotation), glm::value_ptr(m_scale));
 		}
 
 		vec2f scenePos = p_window->m_render_pipeline->GetScenePos();
 		vec2f sceneSize = p_window->m_render_pipeline->GetSceneSize();
 		ImGuizmo::SetRect(scenePos.x, 0.0f, sceneSize.x, sceneSize.y);
 
-		//ImGuizmo::DrawCubes(p_camera_view, p_camera_projection, &objectMatrix [0][0], gizmoCount);
 		float deltaMatrix = 0.1f;
 		ImGuizmo::Manipulate(p_camera_view, p_camera_projection, m_current_gizmo_operation, currentGizmoMode, p_matrix, NULL, useSnap ? &snap [0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
 
@@ -71,7 +57,6 @@ namespace Spore
 		}
 
 		//ImGuizmo::ViewManipulate(p_camera_view, 1.0f, ImVec2(scenePos.x, 0.0f), ImVec2(128, 128), 0x10101010);
-		//ImGuizmo::ViewManipulate(p_camera_view, 8.0f, ImVec2(viewManipulateRight - 128, viewManipulateTop), ImVec2(128, 128), 0x10101010);
 		ImGuizmo::DecomposeMatrixToComponents(p_matrix, glm::value_ptr(m_position), glm::value_ptr(m_rotation), glm::value_ptr(m_scale));
 		ImGuizmo::RecomposeMatrixFromComponents(glm::value_ptr(m_position), glm::value_ptr(m_rotation), glm::value_ptr(m_scale), p_matrix);
 	}
