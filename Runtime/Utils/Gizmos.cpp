3951e3c475c9bad33ee0e708a19a0b49f5715ac8
Cheesecuter <Cheesecuter777@gmail.com>
2024-04-16 16:31:24 +0800

    Add Gizmo Transform Control.

--- Runtime/Utils/Gizmos.cpp
+++ Runtime/Utils/Gizmos.cpp
@@ -67,10 +67,11 @@ namespace Spore
 		if (ImGuizmo::IsUsing())
 		{
 			ImGuizmo::Manipulate(p_camera_view, p_camera_projection, m_current_gizmo_operation, currentGizmoMode, p_matrix, NULL, useSnap ? &snap [0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
+
 		}
-		
+
+		//ImGuizmo::ViewManipulate(p_camera_view, 1.0f, ImVec2(scenePos.x, 0.0f), ImVec2(128, 128), 0x10101010);
 		//ImGuizmo::ViewManipulate(p_camera_view, 8.0f, ImVec2(viewManipulateRight - 128, viewManipulateTop), ImVec2(128, 128), 0x10101010);
-		ImGuizmo::ViewManipulate(p_camera_view, 1.0f, ImVec2(scenePos.x, 0.0f), ImVec2(128, 128), 0x10101010);
 		ImGuizmo::DecomposeMatrixToComponents(p_matrix, glm::value_ptr(m_position), glm::value_ptr(m_rotation), glm::value_ptr(m_scale));
 		ImGuizmo::RecomposeMatrixFromComponents(glm::value_ptr(m_position), glm::value_ptr(m_rotation), glm::value_ptr(m_scale), p_matrix);
 	}
