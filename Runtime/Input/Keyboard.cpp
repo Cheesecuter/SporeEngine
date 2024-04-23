205852d7e0b7bf797e402ac0ab10e3a5e810377c
Cheesecuter <Cheesecuter777@gmail.com>
2024-04-23 22:49:41 +0800

    Modified process input function.

--- Runtime/Input/Keyboard.cpp
+++ Runtime/Input/Keyboard.cpp
@@ -14,35 +14,35 @@ namespace Spore
     void Keyboard::processInput(Spore::MainWindow* p_window,
                                 Camera& p_camera, float32 p_deltaTime)
     {
-        if (glfwGetKey(p_window->m_window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(p_window->m_window, true);
+        if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(p_window->GetWindow(), true);
 
-        if (glfwGetKey(p_window->m_window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+        if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
         {
-            if (glfwGetKey(p_window->m_window, GLFW_KEY_W) == GLFW_PRESS)
+            if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_W) == GLFW_PRESS)
                 p_camera.ProcessKeyboard(FORWARD, p_deltaTime * 2);
-            if (glfwGetKey(p_window->m_window, GLFW_KEY_S) == GLFW_PRESS)
+            if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_S) == GLFW_PRESS)
                 p_camera.ProcessKeyboard(BACKWARD, p_deltaTime * 2);
-            if (glfwGetKey(p_window->m_window, GLFW_KEY_A) == GLFW_PRESS)
+            if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_A) == GLFW_PRESS)
                 p_camera.ProcessKeyboard(LEFT, p_deltaTime * 2);
-            if (glfwGetKey(p_window->m_window, GLFW_KEY_D) == GLFW_PRESS)
+            if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_D) == GLFW_PRESS)
                 p_camera.ProcessKeyboard(RIGHT, p_deltaTime * 2);
-            if (glfwGetKey(p_window->m_window, GLFW_KEY_E) == GLFW_PRESS)
+            if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_E) == GLFW_PRESS)
                 p_camera.ProcessKeyboard(UP, p_deltaTime * 2);
-            if (glfwGetKey(p_window->m_window, GLFW_KEY_Q) == GLFW_PRESS)
+            if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_Q) == GLFW_PRESS)
                 p_camera.ProcessKeyboard(DOWN, p_deltaTime * 2);
         }
-        if (glfwGetKey(p_window->m_window, GLFW_KEY_W) == GLFW_PRESS)
+        if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_W) == GLFW_PRESS)
             p_camera.ProcessKeyboard(FORWARD, p_deltaTime);
-        if (glfwGetKey(p_window->m_window, GLFW_KEY_S) == GLFW_PRESS)
+        if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_S) == GLFW_PRESS)
             p_camera.ProcessKeyboard(BACKWARD, p_deltaTime);
-        if (glfwGetKey(p_window->m_window, GLFW_KEY_A) == GLFW_PRESS)
+        if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_A) == GLFW_PRESS)
             p_camera.ProcessKeyboard(LEFT, p_deltaTime);
-        if (glfwGetKey(p_window->m_window, GLFW_KEY_D) == GLFW_PRESS)
+        if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_D) == GLFW_PRESS)
             p_camera.ProcessKeyboard(RIGHT, p_deltaTime);
-        if (glfwGetKey(p_window->m_window, GLFW_KEY_E) == GLFW_PRESS)
+        if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_E) == GLFW_PRESS)
             p_camera.ProcessKeyboard(UP, p_deltaTime);
-        if (glfwGetKey(p_window->m_window, GLFW_KEY_Q) == GLFW_PRESS)
+        if (glfwGetKey(p_window->GetWindow(), GLFW_KEY_Q) == GLFW_PRESS)
             p_camera.ProcessKeyboard(DOWN, p_deltaTime);
 
         /*if (glfwGetKey(window->window, GLFW_KEY_LEFT_ALT) == GLFW_PRESS)
