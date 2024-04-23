c3b4fec8adf14ccae3fed7cab5bd3e0039d24263
Cheesecuter <Cheesecuter777@gmail.com>
2024-04-23 22:42:36 +0800

    Added global delta time.

--- Runtime/Core/Base/Global.cpp
+++ Runtime/Core/Base/Global.cpp
@@ -5,4 +5,5 @@ namespace Spore
 	std::mutex g_global_mutex;
 	bool g_tick_run = false;
 	bool g_tick_stop = true;
+	float iTime = 0.0f;
 }
--- Runtime/Core/Base/Global.hpp
+++ Runtime/Core/Base/Global.hpp
@@ -7,4 +7,5 @@ namespace Spore
 	extern std::mutex g_global_mutex;
 	extern bool g_tick_run;
 	extern bool g_tick_stop;
+	extern float iTime;
 }
