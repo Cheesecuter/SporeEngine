01c3a867e4d14ba2d3a966cf0989c4b4d2aa4ac3
Cheesecuter <Cheesecuter777@gmail.com>
2024-04-25 21:07:38 +0800

    Added board keys definitions.

---
+++ Runtime/Input/Utils/KeyboardKeys.h
@@ -0,0 +1,84 @@
+#pragma once
+
+namespace Spore
+{
+
+#ifndef KEYBOARD_KEYS
+#define KEYBOARD_KEYS
+
+#define KEY_UNKNOWN			-1	/*               */
+#define KEY_ESC				0	/*      Esc      */
+#define KEY_F1				1	/*      F1       */
+#define KEY_F2				2	/*      F2       */
+#define KEY_F3				3	/*      F3       */
+#define KEY_F4				4	/*      F4       */
+#define KEY_F5				5	/*      F5       */
+#define KEY_F6				6	/*      F6       */
+#define KEY_F7				7	/*      F7       */
+#define KEY_F8				8	/*      F8       */
+#define KEY_F9				9	/*      F9       */
+#define KEY_F10				10	/*      F10      */
+#define KEY_F11				11	/*      F11      */
+#define KEY_F12				12	/*      F12      */
+#define KEY_GRAVE_ACCENT	13	/*      ` ~      */
+#define KEY_1				14	/*      1 !      */
+#define KEY_2				15	/*      2 @      */
+#define KEY_3				16	/*      3 #      */
+#define KEY_4				17	/*      4 $      */
+#define KEY_5				18	/*      5 %      */
+#define KEY_6				19	/*      6 ^      */
+#define KEY_7				20	/*      7 &      */
+#define KEY_8				21	/*      8 *      */
+#define KEY_9				22	/*      9 (      */
+#define KEY_0				23	/*      0 )      */
+#define KEY_MINUS			24	/*      - _      */
+#define KEY_EQUAL			25	/*      = +      */
+#define KEY_BACKSPACE		26	/*   Backspace   */
+#define KEY_TAB				27	/*      Tab      */
+#define KEY_Q				28	/*       Q       */
+#define KEY_W				29	/*       W       */
+#define KEY_E				30	/*       E       */
+#define KEY_R				31	/*       R       */
+#define KEY_T				32	/*       T       */
+#define KEY_Y				33	/*       Y       */
+#define KEY_U				34	/*       U       */
+#define KEY_I				35	/*       I       */
+#define KEY_O				36	/*       O       */
+#define KEY_P				37	/*       P       */
+#define KEY_LEFT_BRACKET	38	/*      [ {      */
+#define KEY_RIGHT_BRACKET	39	/*      ] }      */
+#define KEY_RIGHT_BACKSLASH	40	/*      \ |      */
+#define KEY_CAPS_LOCK		41	/*   Caps Lock   */
+#define KEY_A				42	/*       A       */
+#define KEY_S				43	/*       S       */
+#define KEY_D				44	/*       D       */
+#define KEY_F				45	/*       F       */
+#define KEY_G				46	/*       G       */
+#define KEY_H				47	/*       H       */
+#define KEY_J				48	/*       J       */
+#define KEY_K				49	/*       K       */
+#define KEY_L				50	/*       L       */
+#define KEY_SEMICOLON		51	/*      ; :      */
+#define KEY_APOSTROPHE		52	/*      ' "      */
+#define KEY_ENTER			53	/*     Enter     */
+#define KEY_LEFT_SHIFT		54	/*   Left Shift  */
+#define KEY_Z				55	/*       Z       */
+#define KEY_X				56	/*       X       */
+#define KEY_C				57	/*       C       */
+#define KEY_V				58	/*       V       */
+#define KEY_B				59	/*       B       */
+#define KEY_N				60	/*       N       */
+#define KEY_M				61	/*       M       */
+#define KEY_COMMA			62	/*      , <      */
+#define KEY_PERIOD			63	/*      . >      */
+#define KEY_SLASH			64	/*      / ?      */
+#define KEY_RIGHT_SHIFT		65	/*  Right Shift  */
+#define KEY_LEFT_CONTROL	66	/*  Left Control */
+#define KEY_LEFT_ALT		67	/*    Left Alt   */
+#define KEY_SPACE			68	/*     Space     */
+#define KEY_RIGHT_ALT		69	/*   Right Alt   */
+#define KEY_RIGHT_CONTROL	70	/* Right Control */
+
+#endif
+
+}
