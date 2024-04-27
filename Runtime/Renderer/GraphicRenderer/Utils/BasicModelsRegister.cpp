437e93572e933fb345eb9566815e7082a4219947
Cheesecuter <Cheesecuter777@gmail.com>
2024-04-27 16:27:18 +0800

    Modified basic model register.

--- Runtime/Renderer/GraphicRenderer/Utils/BasicModelsRegister.cpp
+++ Runtime/Renderer/GraphicRenderer/Utils/BasicModelsRegister.cpp
@@ -1,15 +1,16 @@
 #include <BasicModelsRegister.hpp>
+#include <Model.hpp>
 
 namespace Spore
 {
 	BasicModelsRegister::BasicModelsRegister()
 	{
-		Model* cubeModel = new Model("./Assets/Models/_basic models/cube.obj");
-		Model* sphereModel = new Model("./Assets/Models/_basic models/sphere.obj");
-		Model* capsuleModel = new Model("./Assets/Models/_basic models/capsule.obj");
-		Model* cylinderModel = new Model("./Assets/Models/_basic models/cylinder.obj");
-		Model* planeModel = new Model("./Assets/Models/_basic models/plane.obj");
-		Model* quadModel = new Model("./Assets/Models/_basic models/plane.obj");
+		Model* cubeModel		= new Model("./Assets/Utils/Models/cube.obj");
+		Model* sphereModel		= new Model("./Assets/Utils/Models/sphere.obj");
+		Model* capsuleModel		= new Model("./Assets/Utils/Models/capsule.obj");
+		Model* cylinderModel	= new Model("./Assets/Utils/Models/cylinder.obj");
+		Model* planeModel		= new Model("./Assets/Utils/Models/plane.obj");
+		Model* quadModel		= new Model("./Assets/Utils/Models/plane.obj");
 	}
 
 	BasicModelsRegister::~BasicModelsRegister()
--- Runtime/Renderer/GraphicRenderer/Utils/BasicModelsRegister.hpp
+++ Runtime/Renderer/GraphicRenderer/Utils/BasicModelsRegister.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <Model.hpp>
-
 namespace Spore
 {
 	class BasicModelsRegister
