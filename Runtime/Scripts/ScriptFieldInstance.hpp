2f275e113b93e1fddb2ad83feba866b3f36b83b7
Cheesecuter <Cheesecuter777@gmail.com>
2024-05-14 16:51:41 +0800

    Removed scriptFieldType and scriptField to scriptHeaders.

--- Runtime/Scripts/ScriptFieldInstance.hpp
+++ Runtime/Scripts/ScriptFieldInstance.hpp
@@ -4,24 +4,6 @@
 
 namespace Spore
 {
-	enum class ScriptFieldType
-	{
-		None = 0,
-		Float, Double,
-		Bool, Char, Byte, Short, Int, Long,
-		UByte, UShort, UInt, ULong,
-		Vector2, Vector3, Vector4,
-		Entity
-	};
-
-	struct ScriptField
-	{
-		ScriptFieldType m_type;
-		std::string m_name;
-
-		MonoClassField* m_class_field;
-	};
-
 	class ScriptFieldInstance
 	{
 	public:
