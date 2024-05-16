696b804985f00eeeea89a96491b50cdd6a993fbe
Cheesecuter <Cheesecuter777@gmail.com>
2024-05-16 15:58:05 +0800

    Adjusted comments to enhance code readability and maintainability.

--- Runtime/ECS/Component/CharacterControllerComponent.hpp
+++ Runtime/ECS/Component/CharacterControllerComponent.hpp
@@ -9,7 +9,22 @@ namespace Spore
 	public:
 		CharacterControllerComponent();
 		virtual ~CharacterControllerComponent();
+
+	public:
+		/**
+		 * @brief Render the inspector panel for the character controller component.
+		 *
+		 * This function renders the inspector panel for the character controller component using ImGui.
+		 * It provides information about the character controller.
+		 */
 		virtual void InspectorPanel() override;
+		/**
+		 * @brief Update the character controller component's state.
+		 *
+		 * This function updates the character controller component's state based on user input.
+		 *
+		 * @param p_delta_time The time elapsed since the last update.
+		 */
 		virtual void Tick(float32 p_delta_time) override;
 
 	protected:
