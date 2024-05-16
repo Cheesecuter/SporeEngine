2528579117f7cc0f1376061b13da4f174f55ebe0
Cheesecuter <Cheesecuter777@gmail.com>
2024-05-16 15:58:18 +0800

    Adjusted comments to enhance code readability and maintainability.

--- Runtime/ECS/Component/IDComponent.hpp
+++ Runtime/ECS/Component/IDComponent.hpp
@@ -12,13 +12,39 @@ namespace Spore
 		IDComponent();
 		virtual ~IDComponent();
 
-		virtual void InspectorPanel() override;
-		void SetID(uint64 p_uuid);
-		UUID GetID();
+	public:
 
 	protected:
 
 	private:
 		UUID m_UUID;
+
+	public:
+		/**
+		 * @brief Render the inspector panel for the ID component.
+		 *
+		 * This function renders the inspector panel for the ID component using ImGui.
+		 * It displays the UUID associated with the component.
+		 */
+		virtual void InspectorPanel() override;
+		/**
+		 * @brief Set the UUID of the ID component.
+		 *
+		 * This function sets the UUID of the ID component to the specified value.
+		 *
+		 * @param p_uuid The UUID to set.
+		 */
+		void SetID(uint64 p_uuid);
+		/**
+		 * @brief Get the UUID of the ID component.
+		 *
+		 * @return The UUID of the ID component.
+		 */
+		UUID GetID();
+
+	protected:
+
+	private:
+
 	};
 }
