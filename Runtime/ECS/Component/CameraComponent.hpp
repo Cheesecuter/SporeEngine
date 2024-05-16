a187fba4d5aaca805d22f4a91248fc3af3f2440d
Cheesecuter <Cheesecuter777@gmail.com>
2024-05-16 15:57:55 +0800

    Adjusted comments to enhance code readability and maintainability.

--- Runtime/ECS/Component/CameraComponent.hpp
+++ Runtime/ECS/Component/CameraComponent.hpp
@@ -12,10 +12,8 @@ namespace Spore
 	public:
 		CameraComponent();
 		virtual ~CameraComponent();
-		virtual void InspectorPanel() override;
-		virtual void Tick(float32 p_delta_time) override;
-		void SetCamera(Camera* p_camera);
-		Camera* GetCamera();
+
+	public:
 
 	protected:
 
@@ -25,5 +23,41 @@ namespace Spore
 		float32 m_y_offset = 3.0f;
 		float32 m_z_offset = 3.0f;
 		Texture* m_button_image_reset;
+
+	public:
+		/**
+		 * @brief Render the inspector panel for the camera component.
+		 *
+		 * This function renders the inspector panel for the camera component using ImGui.
+		 * It allows users to modify the camera offset.
+		 */
+		virtual void InspectorPanel() override;
+		/**
+		 * @brief Update the camera component's state.
+		 *
+		 * This function updates the camera component's position based on the position of the associated transform component.
+		 *
+		 * @param p_delta_time The time elapsed since the last update.
+		 */
+		virtual void Tick(float32 p_delta_time) override;
+		/**
+		 * @brief Set the camera object associated with the camera component.
+		 *
+		 * This function sets the camera object associated with the camera component.
+		 *
+		 * @param p_camera Pointer to the camera object to set.
+		 */
+		void SetCamera(Camera* p_camera);
+		/**
+		 * @brief Get the camera object associated with the camera component.
+		 *
+		 * @return Pointer to the camera object associated with the camera component.
+		 */
+		Camera* GetCamera();
+
+	protected:
+
+	private:
+
 	};
 }
