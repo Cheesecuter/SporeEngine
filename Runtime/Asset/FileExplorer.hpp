b217043d1cde8832c64ea2ab82e972aa5638beb4
Cheesecuter <Cheesecuter777@gmail.com>
2024-05-16 15:56:18 +0800

    Adjusted comments to enhance code readability and maintainability.

--- Runtime/Asset/FileExplorer.hpp
+++ Runtime/Asset/FileExplorer.hpp
@@ -17,6 +17,15 @@ namespace Spore
 	class FileExplorer : public Singleton<FileExplorer>
 	{
 	public:
+		/**
+		 * @brief Explore files in the file system.
+		 *
+		 * This function opens a file explorer dialog to select a file. It sets the selected file path to the provided path pointer.
+		 *
+		 * @param p_window Pointer to the main window.
+		 * @param p_path Pointer to the path where the selected file path will be stored.
+		 * @param p_assets_type Type of assets to filter in the file explorer.
+		 */
 		void Explorer(MainWindow* p_window, std::filesystem::path* p_path, AssetsType p_assets_type);
 
 	protected:
