efc2f144722b3cc7acfdd18b56d397fb1f6bc5ad
Cheesecuter <Cheesecuter777@gmail.com>
2024-05-16 15:58:34 +0800

    Adjusted comments to enhance code readability and maintainability.

--- Runtime/ECS/Component/LightComponent.hpp
+++ Runtime/ECS/Component/LightComponent.hpp
@@ -14,9 +14,47 @@ namespace Spore
 		LightComponent();
 		virtual ~LightComponent();
 
+	public:
+
+	protected:
+
+	private:
+		float m_ambient_ratio = 0.5f;
+		Shader* m_lighting_shader = nullptr;
+	
+	public:
+		/**
+		 * @brief Render the inspector panel for the light component.
+		 *
+		 * This function renders the inspector panel for the light component using ImGui.
+		 * It allows users to modify the ambient light ratio.
+		 */
 		virtual void InspectorPanel() override;
+		/**
+		 * @brief Set the lighting shader for the light component.
+		 *
+		 * This function sets the lighting shader used by the light component for rendering.
+		 *
+		 * @param p_shader Pointer to the lighting shader to set.
+		 */
 		void SetLightingShader(Shader* p_shader);
+		/**
+		 * @brief Get the lighting shader used by the light component.
+		 *
+		 * @return Pointer to the lighting shader used by the light component.
+		 */
 		Shader* GetLightingShader();
+		/**
+		 * @brief Render the light component.
+		 *
+		 * This function renders the light component using the specified camera, projection, view, and model matrices, and light position.
+		 *
+		 * @param p_camera Pointer to the camera used for rendering.
+		 * @param p_projection The projection matrix.
+		 * @param p_view The view matrix.
+		 * @param p_model The model matrix.
+		 * @param p_light_pos The position of the light source.
+		 */
 		void Render(Camera* p_camera, 
 					mat4f p_projection, mat4f p_view, mat4f p_model, 
 					vec3f p_light_pos);
@@ -24,7 +62,6 @@ namespace Spore
 	protected:
 
 	private:
-		float m_ambient_ratio = 0.5f;
-		Shader* m_lighting_shader = nullptr;
+
 	};
 }
