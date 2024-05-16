3de9812beb5d360afd9b25db0cb82a91d9784498
Cheesecuter <Cheesecuter777@gmail.com>
2024-05-16 16:03:40 +0800

    Adjusted comments to enhance code readability and maintainability.

--- Runtime/ECS/Transform.hpp
+++ Runtime/ECS/Transform.hpp
@@ -11,6 +11,7 @@ namespace Spore
 		Transform(const vec3f& p_position, const vec3f& p_rotation, const vec3f& p_scale);
 		~Transform();
 
+	public:
 		vec3f m_front;
 		vec3f m_right;
 		vec3f m_up;
@@ -19,7 +20,24 @@ namespace Spore
 		vec3f m_scale;
 		const vec3f m_world_up = vec3f(0.0f, 1.0f, 0.0f);
 
+	protected:
+
+	private:
+
+	public:
+		/**
+		 * @brief Update the transformation vectors.
+		 *
+		 * This function updates the front, up, and right vectors of the transformation based on the current rotation.
+		 */
 		void UpdateVectors();
+		/**
+		 * @brief Get the transformation matrix.
+		 *
+		 * This function calculates and returns the transformation matrix representing the current transformation.
+		 *
+		 * @return Transformation matrix representing the current transformation.
+		 */
 		mat4x4f GetMatrix() const;
 
 	protected:
