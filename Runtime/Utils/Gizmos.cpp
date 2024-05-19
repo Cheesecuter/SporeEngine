6923f7546662cd6d6826a3c8f98242312b432d1b
Cheesecuter <Cheesecuter777@gmail.com>
2024-05-19 11:13:21 +0800

    Resolved the issue where Gizmos controls could not be used when the Scene panel was open.

--- Runtime/Utils/Gizmos.cpp
+++ Runtime/Utils/Gizmos.cpp
@@ -46,20 +46,16 @@ namespace Spore
 
 		vec2f scenePos = p_window->GetRenderPipeline()->GetScenePos();
 		vec2f sceneSize = p_window->GetRenderPipeline()->GetSceneSize();
-		ImGuizmo::SetRect(scenePos.x, 0.0f, sceneSize.x, sceneSize.y);
+		ImGuizmo::SetRect(scenePos.x, scenePos.y, sceneSize.x, sceneSize.y);
 
-		float deltaMatrix = 0.1f;
 		ImGuizmo::Manipulate(p_camera_view, p_camera_projection, m_current_gizmo_operation, currentGizmoMode, p_matrix, NULL, useSnap ? &snap [0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
 
 		if (ImGuizmo::IsUsing())
 		{
 			ImGuizmo::Manipulate(p_camera_view, p_camera_projection, m_current_gizmo_operation, currentGizmoMode, p_matrix, NULL, useSnap ? &snap [0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
-
 		}
 
-		//ImGuizmo::ViewManipulate(p_camera_view, 1.0f, ImVec2(scenePos.x, 0.0f), ImVec2(128, 128), 0x10101010);
 		ImGuizmo::DecomposeMatrixToComponents(p_matrix, glm::value_ptr(m_position), glm::value_ptr(m_rotation), glm::value_ptr(m_scale));
-		ImGuizmo::RecomposeMatrixFromComponents(glm::value_ptr(m_position), glm::value_ptr(m_rotation), glm::value_ptr(m_scale), p_matrix);
 	}
 
 	vec3f Gizmos::GetPosition()
